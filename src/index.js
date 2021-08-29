26533042f90b29bc888711f684adebdc9751b4c1
Kushal Khare <kushalkhare.official@gmail.com>
2021-08-29 17:18:19 +0530

    PWA enabled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
+
+registerServiceWorker()
